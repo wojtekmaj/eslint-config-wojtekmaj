9eb9504e5a04f0e53209b94155c3ae59247e5341
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-12-31 19:38:41 +0100

    Set parserOptions.ecmaVersion to latest

--- rules/index.js
+++ rules/index.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:import/recommended'],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
