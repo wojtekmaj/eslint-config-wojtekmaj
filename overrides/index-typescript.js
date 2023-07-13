214a7252da88492dbdedc59c6db3fcf733ae009b
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-13 13:38:11 +0200

    Disable @typescript-eslint/consistent-type-definitions in *.d.ts files

--- overrides/index-typescript.js
+++ overrides/index-typescript.js
@@ -1,24 +1,32 @@
 const requireResolveCwd = require('../utils/require-resolve-cwd');
 
-module.exports = {
-  files: ['*.ts', '*.tsx'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/stylistic',
-  ],
-  parser: requireResolveCwd('@typescript-eslint/parser'),
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        ignoreRestSiblings: true,
-      },
+module.exports = [
+  {
+    files: ['*.ts', '*.tsx'],
+    extends: [
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/stylistic',
     ],
-    '@typescript-eslint/no-use-before-define': 'error',
-    'no-unused-vars': 'off',
-    'no-use-before-define': 'off',
+    parser: requireResolveCwd('@typescript-eslint/parser'),
+    plugins: ['@typescript-eslint'],
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/no-use-before-define': 'error',
+      'no-unused-vars': 'off',
+      'no-use-before-define': 'off',
+    },
   },
-};
+  {
+    files: ['*.d.ts'],
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
+    },
+  },
+];
